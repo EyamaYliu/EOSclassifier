8b8f5318cd43a73febb1ed138750436544aec970
EyamaYliu <yliu53501@gmail.com>
2018-02-20 11:50:03 -0500

    Attempt on ML Classification v 1.0

--- Test.py
+++ Test.py
@@ -1,6 +1,8 @@
 import pickle
 import string
-from sklearn import datasets
+from sklearn.svm import LinearSVC
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import  cross_val_predict
 
 filepath = 'sent.data.train'
 abbrev = 'classes/abbrevs'
@@ -73,7 +75,7 @@ try:
 	negLEN = []
 	for each_item in NegWords:
 		length=len(each_item)
-		negLEN.append(length)
+		negLEN.append([length])
 
 	#Check if in abbrev class
 	negINABBREV = []
@@ -188,43 +190,94 @@ try:
 ##################################################
 #	Append all Features together
 
-	SentenceAttributes = []
+	LableClass = []
 	for each_word in Result:
-		SentenceAttributes.append([each_word])
+	#If end of sentence,label '1'. If not,lable '0'
+		if each_word == 'EOS':
+			LableClass.append(1)
+		else:
+			LableClass.append(0)
+			
 
 	cnt = 0
 
-	for each_word in SentenceAttributes:
+	Features = []
+#Init the first attribute of Features to be negative word length
+	for each_item in negLEN:
+		Features.append(each_item)
 
+
+
+	for each_word in Features:
 	#Adding Negative Word attributes
-		each_word.append(negLEN[cnt])
-		each_word.append(negPUNCcheck[cnt])
-		each_word.append(negNUMcheck[cnt])
-		each_word.append(negCAPcheck[cnt])
-		each_word.append(negINABBREV[cnt])
-		each_word.append(negININTERNAL[cnt])
+		each_word.append(int(negPUNCcheck[cnt]))
+		each_word.append(int(negNUMcheck[cnt]))
+		each_word.append(int(negCAPcheck[cnt]))
+		each_word.append(int(negINABBREV[cnt]))
+		each_word.append(int(negININTERNAL[cnt]))
 
 	#Adding Positive Word attributes
-		each_word.append(posLEN[cnt])
-		each_word.append(posPUNCcheck[cnt])
-		each_word.append(posNUMcheck[cnt])
-		each_word.append(posCAPcheck[cnt])
-		each_word.append(posINABBREV[cnt])
-		each_word.append(posININTERNAL[cnt])
+		each_word.append(int(posLEN[cnt]))
+		each_word.append(int(posPUNCcheck[cnt]))
+		each_word.append(int(posNUMcheck[cnt]))
+		each_word.append(int(posCAPcheck[cnt]))
+		each_word.append(int(posINABBREV[cnt]))
+		each_word.append(int(posININTERNAL[cnt]))
 
 	#Adding Next/Prev Delimiter and Spaces follow attributes
-		each_word.append(NextDeli[cnt])
-		each_word.append(PrevDeli[cnt])
-		each_word.append(SpacesFollow[cnt])
-	
+		each_word.append(int(NextDeli[cnt]))
+		each_word.append(int(PrevDeli[cnt]))
+		each_word.append(int(SpacesFollow[cnt]))
 		cnt += 1
 
 ##########################################
-
-		
-
+	
 
 
+	#Separate data with Training and Test groups. Train on 80%, test on 20%
+	numofTrain = int(len(Features)*0.8)
+
+	TrainFeatures = Features[0:numofTrain]
+	TrainLabels = LableClass[0:numofTrain]
+	TestFeatures = Features[numofTrain:len(Features)]
+	TestLables = LableClass[numofTrain:len(Features)]
+	
+
+	#Use LinearSVC to train the model with cross validation
+	EOSClassifier = LinearSVC(random_state = 0)
+	EOSClassifier.fit(TrainFeatures,TrainLabels)
+	#score = cross_val_score(EOSClassifier,TrainFeatures,TrainLabels,cv = 5)
+	
+	
+	#Test accuracy on train data, and predict a label
+	
+	accuracy = EOSClassifier.score(TestFeatures,TestLables)
+	PredictionLabel = EOSClassifier.predict(TestFeatures)
+	print(PredictionLabel)
+	
+	Right = 0
+	Wrong = 0
+	cnt = 0
+
+	"""#Check for accuracy
+	for each_item in predict:
+		if each_item == TestLables[cnt]:
+			Right += 1
+		else:
+			Wrong += 1
+		cnt += 1
+	accuracy = 100*(Right/(Right+Wrong))"""
+
+	print(accuracy)
+	
+	
+
+	
+
+
+	data.close()
+	abbrevclass.close()
+	internalclass.close()
 except IOError as err:
 	print('File error:' + str(err))
 		
